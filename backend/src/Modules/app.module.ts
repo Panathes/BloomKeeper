5deab5a4ff41da445b0a2b9b9d3c977b734ee8a4
Charlebois Christophe <Panathes@users.noreply.github.com>
2025-01-29 22:57:32 +0100

    Change Architecture

--- backend/src/Modules/app.module.ts
+++ backend/src/Modules/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from '../Controllers/app.controller';
+import { AppService } from '../Providers/app.service';
 
 @Module({
   imports: [],
